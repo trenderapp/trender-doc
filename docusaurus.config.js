ae922896b96751b7ed70b7443fd72449dba80b9c
alex_0ff <35507235+alexandrecoulay@users.noreply.github.com>
2023-09-20 11:50:13 +0000

    Add Github info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,14 +10,16 @@ const config = {
   tagline: 'Official Trender Social Network documentation',
   url: 'https://doc.trenderapp.com',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'trenderapp', // Usually your GitHub org/user name.
   projectName: 'trender-doc', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
